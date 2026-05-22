e4779cdee9e1174b458fef727a0b9edc57a4a7f5
WinnieW685 <wwu82648@gmail.com>
2026-05-22 12:32:04 -0400

    dd

--- garden/src/views/LoginPage.vue
+++ garden/src/views/LoginPage.vue
@@ -1,7 +1,35 @@
 <template>
-  <div></div>
+  <div><h1>Login</h1></div>
 </template>
 
-<script setup></script>
+<script setup>
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+const pinia = createPinia()
+const app = createApp(App)
+
+import { ref } from 'vue'
+import { supabase } from '../supabase'
+const loading = ref(false)
+const email = ref('')
+const handleLogin = async () => {
+  try {
+    loading.value = true
+    const { error } = await supabase.auth.signInWithOtp({ email: email.value })
+    if (error) throw error
+    alert('Check your email for the login link!')
+  } catch (error) {
+    if (error instanceof Error) {
+      alert(error.message)
+    }
+  } finally {
+    loading.value = false
+  }
+}
+
+app.use(pinia)
+app.mount('#app')
+</script>
 
 <style lang="scss" scoped></style>
