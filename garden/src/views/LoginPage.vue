2b0bb078f352a92b4addd542f2cf0dc27d81d2aa
WinnieW685 <wwu82648@gmail.com>
2026-05-26 12:31:37 -0400

    aaaa

--- garden/src/views/LoginPage.vue
+++ garden/src/views/LoginPage.vue
@@ -1,18 +1,30 @@
 <template>
-  <div><h1>Login</h1></div>
+  <form class="row flex-center flex" @submit.prevent="handleLogin">
+    <div class="col-6 form-widget">
+      <h1 class="header">Supabase + Vue 3</h1>
+      <p class="description">Sign in via magic link with your email below</p>
+      <div>
+        <input class="inputField" type="email" placeholder="Your email" v-model="email" />
+      </div>
+      <div>
+        <input
+          type="submit"
+          class="button block"
+          :value="loading ? 'Loading' : 'Send magic link'"
+          :disabled="loading"
+        />
+      </div>
+    </div>
+  </form>
 </template>
 
 <script setup>
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-const pinia = createPinia()
-const app = createApp(App)
-
 import { ref } from 'vue'
 import { supabase } from '../supabase'
+
 const loading = ref(false)
 const email = ref('')
+
 const handleLogin = async () => {
   try {
     loading.value = true
@@ -27,9 +39,6 @@ const handleLogin = async () => {
     loading.value = false
   }
 }
-
-app.use(pinia)
-app.mount('#app')
 </script>
 
 <style lang="scss" scoped></style>
