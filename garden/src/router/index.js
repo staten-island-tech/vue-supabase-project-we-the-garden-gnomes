a91251177f7ed828702d738a184a0eaaee9856cd
JoannaZhang-cs <jzsunset10@gmail.com>
2026-05-28 12:12:06 -0400

    water component

--- garden/src/router/index.js
+++ garden/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import WaterView from '../views/water.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -6,40 +7,9 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: MyGarden,
+      component: WaterView,
     },
   ],
 })
 
 export default router
-
-
-
-let balance = 0.00;
-let waterAmount = 3 ;
-
-const waterBtn = document.getElementById('waterBtn');
-const walletDisplay = document.getElementById('wallet');
-const waterDisplay = document.getElementById('water');
-const flowerDisplay = document.getElementById('flower');
-
-//pay amount per button press
-const payout = 0.10; 
-
-waterBtn.addEventListener('click', () => {
-  balance += payout;
-  walletDisplay.textContent = `Balance: $${balance.toFixed(2)}`;
-
-  //watering
-
-  if (waterAmount < 100) {
-    waterPercentage += 10;
-    waterDisplay.style.height = `{waterAmount}`;
-  }
-
-  //animating flower height
-
-  setTimeout(() => {
-    flowerDisplay.style.transform = 'scale(1)';
-  }, 300);
-});
