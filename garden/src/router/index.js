b3e066f74c5d5cd1c4132ac206f0fd9b93f43c8d
YuXuanDevanLin <lin13665010834@gmail.com>
2026-06-01 12:20:41 -0400

    gggczxc

--- garden/src/router/index.js
+++ garden/src/router/index.js
@@ -1,13 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import GardenView from '../views/garden.vue'
-
+import LoginPage from '@/views/LoginPage.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/aa',
       name: 'home',
       component: GardenView,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginPage,
     },
   ],
 })
