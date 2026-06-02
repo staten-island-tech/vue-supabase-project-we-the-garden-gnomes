938bebe13ae15529de971113524cf8a5cc692ae7
JoannaZhang-cs <jzsunset10@gmail.com>
2026-06-02 12:08:48 -0400

    Update index.js

--- garden/src/router/index.js
+++ garden/src/router/index.js
@@ -5,7 +5,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/aa',
+      path: '/',
       name: 'home',
       component: GardenView,
     },
