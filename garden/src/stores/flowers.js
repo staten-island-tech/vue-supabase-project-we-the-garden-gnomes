4aa6c2f1c8044124f3de2b628176d6eabfe8c0b2
YuXuanDevanLin <lin13665010834@gmail.com>
2026-06-02 12:09:16 -0400

    deu

--- garden/src/stores/flowers.js
+++ garden/src/stores/flowers.js
@@ -3,17 +3,17 @@ import { defineStore } from 'pinia'
 
 export const useFlowerStore = defineStore('flower', () => {
   const flowers = ref([
-    { name: 'Roses', buyPrice: 10, sellPrice: 15, imageUrl: '' },
-    { name: 'Tulips', buyPrice: 8, sellPrice: 12, imageUrl: '' },
-    { name: 'Sunflowers', buyPrice: 10, sellPrice: 15, imageUrl: '' },
-    { name: 'Orchids', buyPrice: 8, sellPrice: 12, imageUrl: '' },
-    { name: 'Lilies', buyPrice: 12, sellPrice: 18, imageUrl: '' },
-    { name: 'Carnations', buyPrice: 6, sellPrice: 9, imageUrl: '' },
-    { name: 'Hydrangeas', buyPrice: 16, sellPrice: 24, imageUrl: '' },
-    { name: 'Peonies', buyPrice: 8, sellPrice: 12, imageUrl: '' },
-    { name: 'Daisies', buyPrice: 6, sellPrice: 9, imageUrl: '' },
-    { name: 'Chrysanthemums', buyPrice: 20, sellPrice: 30, imageUrl: '' },
-    { name: 'Goldfish Plants', buyPrice: 50, sellPrice: 75, imageUrl: '' },
+    { name: 'Roses', buyPrice: 10, sellPrice: 15, imageUrl: '/public/Rose.png' },
+    { name: 'Tulips', buyPrice: 8, sellPrice: 12, imageUrl: '/public/Tulip.png' },
+    { name: 'Sunflowers', buyPrice: 10, sellPrice: 15, imageUrl: '/public/Sunflower.png' },
+    { name: 'Orchids', buyPrice: 8, sellPrice: 12, imageUrl: '/public/Orchid.png' },
+    { name: 'Lilies', buyPrice: 12, sellPrice: 18, imageUrl: '/public/Lily.png' },
+    { name: 'Carnations', buyPrice: 6, sellPrice: 9, imageUrl: '/public/Carnation.png' },
+    { name: 'Hydrangeas', buyPrice: 16, sellPrice: 24, imageUrl: '/public/Hydrangea.png' },
+    { name: 'Peonies', buyPrice: 8, sellPrice: 12, imageUrl: '/public/Peony.png' },
+    { name: 'Daisies', buyPrice: 6, sellPrice: 9, imageUrl: '/public/Daisy.png' },
+    { name: 'Chrysanthemums', buyPrice: 20, sellPrice: 30, imageUrl: '/public/Chrysanthemum.png' },
+    { name: 'Goldfish Flower', buyPrice: 50, sellPrice: 75, imageUrl: '/public/GoldfishFlower.png' },
   ])
 
   return {
