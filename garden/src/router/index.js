aaa9b3148ad7322156c7ad068b6a6c3bb42a51b0
JoannaZhang-cs <jzsunset10@gmail.com>
2026-06-02 12:13:19 -0400

    Revert "Merge branch 'main' into JoannaZhangBranch"
    This reverts commit 316fca35931f1332dac2e9dd11e15d17a1816187, reversing
    changes made to dcebde4a522e51f5b0599828d992b843533cf9da.

--- garden/src/router/index.js
+++ garden/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import GardenView from '../views/garden.vue'
-import LoginPage from '@/views/LoginPage.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -8,11 +8,6 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: GardenView,
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: LoginPage,
     },
   ],
 })
