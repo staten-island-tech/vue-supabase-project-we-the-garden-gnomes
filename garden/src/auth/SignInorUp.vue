37d8d6332a64f0e8dcd3e9925de362ac6bc8ca33
YuXuanDevanLin <lin13665010834@gmail.com>
2026-06-16 12:06:47 -0400

    gg

--- garden/src/auth/SignInorUp.vue
+++ garden/src/auth/SignInorUp.vue
@@ -3,7 +3,11 @@
     <h2 class="color-black">Sign In/Up</h2>
     <form @submit.prevent="handleRegister">
       <input v-model="email" type="email" placeholder="example@example.com" required />
-      <button type="submit">Sign In</button>
+      <button
+              type="submit"
+              :class="{ loading: loading }"
+              z              {{ loading ? 'Sending Magic Link...' : 'Sign In' }}
+            </button>
     </form>
   </div>
 </template>
