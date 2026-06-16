b675227eea354d67cc6e3a507cc5dc143491cf23
YuXuanDevanLin <lin13665010834@gmail.com>
2026-06-15 22:29:08 -0400

    Update garden.vue

--- garden/src/views/garden.vue
+++ garden/src/views/garden.vue
@@ -228,9 +228,9 @@ onUnmounted(() => {
 
 .bg-inventory {
   position: fixed;
-  top: 0rem;
-  right: 12rem;
-  width: 480px;
+  top: 1%;
+  right: 10%;
+  width: 26%;
   max-width: 30vw;
   z-index: 30;
   pointer-events: auto;
